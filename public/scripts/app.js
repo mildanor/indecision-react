e54022b066a43ad7e83db95d3b24436636d9f5ec
milda norkute <menulio.kiskis@gmail.com>
2018-04-20 18:26:41 +0200

    second challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,14 @@ console.log('App js is running');
 //app.js in public folder will change automatically for us
 //Babel should convert the jsx which browsers doesnt understand
 
-//Taks: Create app object title/subtitle, title subititle, render
+//Task 1: Create app object title/subtitle, title subititle, render
+//Task 2:
+//1) only render subtitle if it exists with logical &&
+//2) if options.length > 0 show "Options" else message "No options" with ternery operator
 var object = {
     title: 'Indecision App ',
-    subtitle: 'Let computer decide'
+    subtitle: 'Let computer decide',
+    options: ['One', 'Two']
 };
 var template = React.createElement(
     'div',
@@ -17,15 +21,17 @@ var template = React.createElement(
         null,
         object.title
     ),
-    React.createElement(
+    object.subtitle && React.createElement(
         'p',
         null,
+        ' ',
         object.subtitle
     ),
     React.createElement(
         'p',
         null,
-        'static data example'
+        object.options.length > 0 ? "Options are: " : "No options",
+        ' '
     )
 );
 
@@ -35,39 +41,39 @@ var userName = Milda;
 var userAge = 27;
 var userLocation = 'Vilnius';
 */
-
+//true ? 'Milda : 'Anonymous - second is if condition doesnt pass
 //or you can have an object
 var user = {
     name: 'Milda',
     age: 26,
-    location: 'Geneva',
-    food: 'pizza'
+    location: 'Geneva'
 };
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name.toUpperCase()
+        user.name ? user.name.toUpperCase() : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
-        'Age:',
+        ' Age: ',
         user.age
     ),
-    React.createElement('ol', null),
-    React.createElement(
-        'li',
-        null,
-        user.location
-    ),
-    React.createElement(
-        'li',
-        null,
-        user.food
-    )
+    getLocation(user.location)
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ console.log('App js is running');
 //app.js in public folder will change automatically for us
 //Babel should convert the jsx which browsers doesnt understand
 
-//Taks: Create app object title/subtitle, title subititle, render
+//Task 1: Create app object title/subtitle, title subititle, render
+//Task 2:
+//1) only render subtitle if it exists with logical &&
+//2) if options.length > 0 show "Options" else message "No options" with ternery operator
 var object={
     title:'Indecision App ',
     subtitle:'Let computer decide',
-}
+    options: ['One', 'Two']
+};
 var template = (
 <div>
 <h1>{object.title}</h1>
-<p>{object.subtitle}</p>
-<p>static data example</p>
+{object.subtitle && <p> {object.subtitle}</p>}
+<p>{object.options.length > 0 ? "Options are: " : "No options" } </p>
 </div>
 );
 
@@ -22,21 +26,24 @@ var userName = Milda;
 var userAge = 27;
 var userLocation = 'Vilnius';
 */
-
+//true ? 'Milda : 'Anonymous - second is if condition doesnt pass
 //or you can have an object
 var user={
     name:'Milda',
     age:26,
-    location:'Geneva',
-    food:'pizza'
+    location:'Geneva'
 }
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>;
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age:{user.age}</p>
-        <ol></ol>
-        <li>{user.location}</li>
-        <li>{user.food}</li>
+        <h1>{user.name ? user.name.toUpperCase(): 'Anonymous'}</h1>
+        {user.age && user.age >= 18 && <p> Age: {user.age}</p>}
+        {getLocation(user.location)}
         </div>
     );
 var appRoot = document.getElementById('app');
