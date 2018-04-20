87ec2a1921bb477857ee23b699208802b2afad78
milda norkute <menulio.kiskis@gmail.com>
2018-04-20 16:04:59 +0200

    server issue solved

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # indecision-react
 
 example app: http://indecision.mead.io/
+
+To see live changes run: babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch and live-server public from two dfferent terminals of pc
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,12 +3,73 @@
 console.log('App js is running');
 //app.js in public folder will change automatically for us
 //Babel should convert the jsx which browsers doesnt understand
+
+//Taks: Create app object title/subtitle, title subititle, render
+var object = {
+    title: 'Indecision App ',
+    subtitle: 'Let computer decide'
+};
 var template = React.createElement(
-  'p',
-  null,
-  'Title'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        object.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        object.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        'static data example'
+    )
+);
+
+/*
+Inject data
+var userName = Milda;
+var userAge = 27;
+var userLocation = 'Vilnius';
+*/
+
+//or you can have an object
+var user = {
+    name: 'Milda',
+    age: 26,
+    location: 'Geneva',
+    food: 'pizza'
+};
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name.toUpperCase()
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:',
+        user.age
+    ),
+    React.createElement('ol', null),
+    React.createElement(
+        'li',
+        null,
+        user.location
+    ),
+    React.createElement(
+        'li',
+        null,
+        user.food
+    )
 );
 var appRoot = document.getElementById('app');
 
-//run this template (forst arguement) in this element (second argument)
+//run this template (first arguement) in this element (second argument)
 ReactDOM.render(template, appRoot);
