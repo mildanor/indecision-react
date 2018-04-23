c9fa55e1f28a122141b319bde49c7310b8d094ee
milda norkute <menulio.kiskis@gmail.com>
2018-04-23 18:06:59 +0200

    second arrow function challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*ES 5
 const square = function (x) {
@@ -49,3 +49,64 @@ if (fullName){
 }
 */
 console.log(firstName("Milda Norkute"));
+
+//////// SECOND PART
+
+//arguments object is not bound with arrow functions
+//THIS keyword is not bound in arrow functions
+/*
+const add = function (a,b){
+    console.log(arguments);
+    return a+b;
+};
+*/
+
+/*
+const add = (a,b) =>{
+    return a+b;
+}
+console.log(add(55,1, 10, 20));
+*/
+
+var user = {
+    name: 'Milda',
+    cities: ['Vilnius', 'Ferney', 'London'],
+    //Better to use ES 5 inside object - as THIS value is bound to object property
+    // printplacesLived: function (){
+    // console.log(this.name);
+    // console.log(this.cities);
+    /*
+     const that = this;
+     that.cities.forEach(function(city){
+         console.log(that.name + ' has lived in ' + city);
+     });
+     */
+    printplacesLived: function printplacesLived() {
+        var _this = this;
+
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived ' + city;
+        });
+        /*
+         this.cities.forEach((city)=> {
+           console.log(this.name + ' has lived in ' + city);
+         });
+          */
+    }
+};
+console.log(user.printplacesLived());
+
+//CHALLENGE 
+var multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return _this2.multiplyBy * number;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
--- src/playground/arrow-functions.js
+++ src/playground/arrow-functions.js
@@ -43,3 +43,57 @@ if (fullName){
 }
 */
 console.log(firstName("Milda Norkute")); 
+
+//////// SECOND PART
+
+//arguments object is not bound with arrow functions
+//THIS keyword is not bound in arrow functions
+/*
+const add = function (a,b){
+    console.log(arguments);
+    return a+b;
+};
+*/
+
+
+/*
+const add = (a,b) =>{
+    return a+b;
+}
+console.log(add(55,1, 10, 20));
+*/
+
+const user = {
+    name: 'Milda',
+    cities: ['Vilnius', 'Ferney', 'London'],
+    //Better to use ES 5 inside object - as THIS value is bound to object property
+   // printplacesLived: function (){
+       // console.log(this.name);
+       // console.log(this.cities);
+       /*
+        const that = this;
+        that.cities.forEach(function(city){
+            console.log(that.name + ' has lived in ' + city);
+        });
+        */
+        printplacesLived(){
+       return this.cities.map((city) => this.name + ' has lived '+ city);
+       /*
+        this.cities.forEach((city)=> {
+          console.log(this.name + ' has lived in ' + city);
+        });
+         */
+    }
+};
+console.log(user.printplacesLived());
+
+//CHALLENGE 
+const multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map((number) => this.multiplyBy * number);
+    }
+};
+
+console.log(multiplier.multiply());
